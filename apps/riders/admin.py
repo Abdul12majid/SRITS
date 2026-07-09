26dbd458877ce21d4da498487f056e3b33e8df7b
Abdul12majid <yisaabdulmajid@gmail.com>
2026-07-09 21:34:03 +0100

    created rider model

--- apps/riders/admin.py
+++ apps/riders/admin.py
@@ -1,3 +1,56 @@
 from django.contrib import admin
+from .models import Rider, NextOfKin, Motorcycle
 
-# Register your models here.
+
+@admin.register(Rider)
+class RiderAdmin(admin.ModelAdmin):
+    list_display = (
+        "first_name",
+        "last_name",
+        "phone_number",
+        "nin",
+        "state_of_origin",
+    )
+
+    search_fields = (
+        "first_name",
+        "last_name",
+        "phone_number",
+        "nin",
+    )
+
+    list_filter = (
+        "gender",
+        "state_of_origin",
+    )
+
+
+@admin.register(NextOfKin)
+class NextOfKinAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "phone_number",
+        "relationship",
+        "rider",
+    )
+
+    search_fields = (
+        "name",
+        "phone_number",
+    )
+
+
+@admin.register(Motorcycle)
+class MotorcycleAdmin(admin.ModelAdmin):
+    list_display = (
+        "plate_number",
+        "brand",
+        "color",
+        "rider",
+    )
+
+    search_fields = (
+        "plate_number",
+        "engine_number",
+        "chassis_number",
+    )
--- apps/riders/models.py
+++ apps/riders/models.py
@@ -1,3 +1,86 @@
 from django.db import models
+from apps.common.models import BaseModel
 
-# Create your models here.
+
+class Rider(BaseModel):
+    GENDER_CHOICES = (
+        ("male", "Male"),
+        ("female", "Female"),
+    )
+
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    middle_name = models.CharField(max_length=100, blank=True)
+
+    date_of_birth = models.DateField()
+
+    gender = models.CharField(
+        max_length=10,
+        choices=GENDER_CHOICES
+    )
+
+    phone_number = models.CharField(
+        max_length=15,
+        unique=True
+    )
+
+    nin = models.CharField(
+        max_length=11,
+        unique=True
+    )
+
+    address = models.TextField()
+
+    state_of_origin = models.CharField(max_length=100)
+
+    lga = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class NextOfKin(BaseModel):
+    rider = models.OneToOneField(
+        Rider,
+        on_delete=models.CASCADE,
+        related_name="next_of_kin"
+    )
+
+    name = models.CharField(max_length=200)
+
+    phone_number = models.CharField(max_length=15)
+
+    relationship = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Motorcycle(BaseModel):
+    rider = models.OneToOneField(
+        Rider,
+        on_delete=models.CASCADE,
+        related_name="motorcycle"
+    )
+
+    plate_number = models.CharField(
+        max_length=20,
+        unique=True
+    )
+
+    chassis_number = models.CharField(
+        max_length=100,
+        unique=True
+    )
+
+    engine_number = models.CharField(
+        max_length=100,
+        unique=True
+    )
+
+    brand = models.CharField(max_length=100)
+
+    color = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.plate_number
